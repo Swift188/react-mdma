bbaac6279a70470e91b5c40dc1c0bd012fd846ec
Swift <538464@edu.rocmn.nl>
2021-09-29 12:39:29 +0200

    creating-playlists-sucks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,91 +1,114 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect, useHistory } from 'react-router-dom';
 // import AddPlaylistModal from '../components/AddPlaylistModal';
 // import PropTypes from 'prop-types';
 import Playlist from '../components/Playlist';
 
 const Home = () => {
-	// const [modalOpen, setModalOpen] = useState(false);
-	const [search, setSearch] = useState('');
-	const [searchResults, setSearchResults] = useState([]);
-	const [playlists, setPlaylists] = useState([]);
+  // const [modalOpen, setModalOpen] = useState(false);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlists, setPlaylists] = useState([]);
+  const history = useHistory();
 
-	useEffect(async () => {
-		if (localStorage.getItem('playlists') == null) {
-			let res = await fetch('http://localhost:3001/playlists');
-			let json = await res.json();
-			setPlaylists(json);
-		} else {
-			setPlaylists(JSON.parse(localStorage.getItem('playlists')));
-		}
-	}, []);
+  useEffect(async () => {
+    // if (localStorage.getItem('playlists') == null) {
+    //   let res = await fetch('http://localhost:3001/playlists');
+    //   let json = await res.json();
+    //   setPlaylists(json);
+    // } else {
+    //   setPlaylists(JSON.parse(localStorage.getItem('playlists')));
+    // }
+    await getPlaylists();
+  }, []);
 
-	function togglePinned(e, id) {
-		let updated = playlists.map((playlist) => {
-			if (playlist.id == id)
-				return { ...playlist, pinned: !playlist.pinned };
-			return playlist;
-		});
-		updated.sort((a, b) => {
-			if (a.pinned > b.pinned) return -1;
-			if (b.pinned > a.pinned) return 1;
-			return 0;
-		});
-		setPlaylists(updated);
-		localStorage.setItem('playlists', JSON.stringify(updated));
-	}
+  useEffect(() => {
+    setSearchResults(playlists);
+  }, [playlists]);
 
-	useEffect(() => {
-		setSearchResults(playlists);
-	}, [playlists]);
+  useEffect(() => {
+    setSearchResults(
+      playlists.filter((playlist) =>
+        playlist.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search]);
 
-	useEffect(() => {
-		setSearchResults(
-			playlists.filter((playlist) =>
-				playlist.name.toLowerCase().includes(search.toLowerCase())
-			)
-		);
-	}, [search]);
+  function togglePinned(e, id) {
+    let updated = playlists.map((playlist) => {
+      if (playlist.id == id) return { ...playlist, pinned: !playlist.pinned };
+      return playlist;
+    });
+    updated.sort((a, b) => {
+      if (a.pinned > b.pinned) return -1;
+      if (b.pinned > a.pinned) return 1;
+      return 0;
+    });
+    setPlaylists(updated);
+    // localStorage.setItem('playlists', JSON.stringify(updated));
+  }
 
-	return (
-		<div className='container'>
-			{/* {loggedIn ? <button onClick={logout}>Logout</button> : <button onClick={toggleLogin}>Login</button>} */}
+  async function getPlaylists() {
+    let res = await fetch('http://localhost:3001/playlists');
+    let json = await res.json();
+    setPlaylists(json);
+    // localStorage.setItem('playlists', JSON.stringify(json));
+  }
 
-			{/* {loginVisible == true & !loggedIn && <Login setUser={setUser} setLoggedIn={setLoggedIn}/>} */}
-			{/* {modalOpen && <AddPlaylistModal />} */}
+  const createPlaylist = async () => {
+    const res = await fetch('http://localhost:3001/playlists/', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'New playlist',
+        icon: '',
+        pinned: false,
+        creator: 'You',
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const json = await res.json();
+    console.log(`/playlist/${json.id}`);
+    history.push(`/playlist/${json.id}`);
+  };
 
-			<div className='search-bar'>
-				<input
-					value={search}
-					type='text'
-					className='search-input'
-					placeholder='Search'
-					onChange={(e) => setSearch(e.target.value)}
-				/>
-				<Link
-					to='/playlists/create'
-					// onClick={(e) => setModalOpen(!modalOpen, e)}
-					className='add-playlist-btn'
-					href='#'
-				>
-					<img src='/img/plus-2.png' alt='Add song' />
-				</Link>
-			</div>
+  return (
+    <div className='container'>
+      {/* {loggedIn ? <button onClick={logout}>Logout</button> : <button onClick={toggleLogin}>Login</button>} */}
 
-			<h1 className='text-center'>Playlists</h1>
-			<div className='splitter'></div>
+      {/* {loginVisible == true & !loggedIn && <Login setUser={setUser} setLoggedIn={setLoggedIn}/>} */}
+      {/* {modalOpen && <AddPlaylistModal />} */}
 
-			<div className='playlist-list'>
-				{searchResults.map((playlist) => (
-					<Playlist
-						key={playlist.id}
-						playlist={playlist}
-						togglePinned={togglePinned}
-					/>
-				))}
-			</div>
-		</div>
-	);
+      <div className='search-bar'>
+        <input
+          value={search}
+          type='text'
+          className='search-input'
+          placeholder='Search'
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <a
+          onClick={() => createPlaylist()}
+          className='add-playlist-btn'
+          href='#'
+        >
+          <img src='/img/plus-2.png' alt='Create new playlist' />
+        </a>
+      </div>
+
+      <h1 className='text-center'>Playlists</h1>
+      <div className='splitter'></div>
+
+      <div className='playlist-list'>
+        {searchResults.map((playlist) => (
+          <Playlist
+            key={playlist.id}
+            playlist={playlist}
+            togglePinned={togglePinned}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 // Home.propTypes = {
